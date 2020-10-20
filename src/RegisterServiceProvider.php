c840a48022a8eec5754df95044bdfbe84a00254c
Azamjon99 <oktamovazam1@gmail.com>
2020-10-20 16:16:50 +0500

    ASV

--- src/RegisterServiceProvider.php
+++ src/RegisterServiceProvider.php
@@ -9,12 +9,10 @@ public function boot(){
      $this->loadRoutesFrom(__DIR__.'/routes/web.php');
      $this->loadViewsFrom(__DIR__.'/views', 'register');
      $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-    $this->mergeConfigFrom(
-        __DIR__.'/config/contact.php', 'contact'
-    );
+   
     
     $this->publishes([
-        __DIR__.'/config/contact.php' => config_path('contact.php'),
+        __DIR__.'/config/register.php' => config_path('register.php'),
     ]);
 }
 
